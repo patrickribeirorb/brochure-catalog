daa372a8b80090249215140120c69d9ccc54eb60
patrickribeirorb <patrickribeiro.rb@gmail.com>
2022-05-29 18:23:14 -0300

    #4 fix eslint config

--- src/App/Home/Home.tsx
+++ src/App/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <h2>Hello</h2>;
+    return <h2>Hello</h2>;
 }
 
 export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
-import App from './App/App';
-import store from './App/store';
-import reportWebVitals from './reportWebVitals';
+import App from "./App/App";
+import store from "./App/store";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
 
 root.render(
     <React.StrictMode>
