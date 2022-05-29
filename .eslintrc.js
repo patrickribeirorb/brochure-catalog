ade179bfaafc4066cb4744bcb6991719d8099fc5
Patrick dos Santos Ribeiro <59751792+patrickribeirorb@users.noreply.github.com>
2022-05-29 18:33:03 -0300

    #4 fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-
-const prettierOptions = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8')
-);
-
 module.exports = {
-  root: true,
-  extends: ['react-app', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        ...prettierOptions,
-        endOfLine: 'auto',
-      },
-    ],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-unused-vars': 'warn',
-  },
-  overrides: [
-    {
-      files: ['**/*.js?(x)', '**/*.ts?(x)'],
-      rules: {
-        'prettier/prettier': ['warn', prettierOptions],
-        'react-hooks/exhaustive-deps': 'warn',
-      },
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-  ],
-};
+    "extends": [
+        "plugin:react/recommended",
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+
+    }
+}
